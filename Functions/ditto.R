e1f57c5c75ae8bfb837e439f1c11db42e2421684
Eugene Olkhov <eugeneolkhov@gmail.com>
2021-02-01 17:00:16 -0500

    add plot cases function

--- Functions/ditto.R
+++ Functions/ditto.R
@@ -1,8 +1,8 @@
 ditto <- function(dist_df, chosen_county, n = 10){
   
   dist_df %>% 
-    filter(county == chosen_county) %>% 
-    gather(comp, distance, -county) %>% 
+    filter(fips == chosen_county) %>% 
+    gather(comp, distance, -fips) %>% 
     filter(comp != chosen_county) %>% 
     arrange(distance) %>% 
     head(n)
---
+++ Functions/plot_cases.R
@@ -0,0 +1,23 @@
+plot_cases <- function(cases_gathered, fips_code){
+  
+  ggplotly(
+  data_aggregated %>% 
+    filter(fips == 1001) %>%
+    ggplot(aes(x = week,
+               y = cases,
+               group = 1,
+               text = paste0("<br><b>Week: </b>", week,
+                             "<br><b>Number of Weekly Cases: </b>", cases))) +
+    geom_line(color = CR_red()) +
+    theme_compassred() +
+    scale_y_continuous(labels = scales::comma) +
+    labs(x = "",
+         y = "Number of Weekly Cases")
+    
+    
+  , tooltip = c("text")) %>% 
+    config(displayModeBar = F)
+  
+  
+  
+}
