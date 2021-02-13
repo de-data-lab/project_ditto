59d011af5eb7c61c9f5eacd2d1dd599119abed48
Ben Kates <bkates@compassred.com>
2021-02-13 16:51:46 -0500

    update resolution of counties/states to 5m

--- data-raw/pull_states.R
+++ data-raw/pull_states.R
@@ -1,4 +1,4 @@
-states <- tigris::states(cb = T) %>% 
+states <- tigris::states(cb = T,resolution = "5m") %>% 
   select(NAME,STATEFP) %>% 
   filter(NAME != "Commonwealth of the Northern Mariana Islands")
 saveRDS(states,"data/states.RDS")
