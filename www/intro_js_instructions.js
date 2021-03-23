6c624500b5067d8d938ba30446ac981b972680bf
Ben Kates <bkates@compassred.com>
2021-03-23 15:44:51 -0400

    Update intro_js_instructions.js

--- www/intro_js_instructions.js
+++ www/intro_js_instructions.js
@@ -1,33 +1,37 @@
 if (localStorage.getItem('doneTour') !== 'yeah!'){
   introJs().setOptions({
   steps: [{
-    title: 'Welcome',
-    intro: 'Hello World! 👋<br>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+    title: 'Welcome 👋',
+    intro: 'Have you ever wondered what other places in the country are experiencing the pandemic in the same way that you are? <br><br>ne way to think about that is to ask "which counties have had the most similar case trends to my county?" Are there other counties in the country whose cases have risen and fallen in the same way that yours has?'
   },
   {
     element: document.querySelector('#county_select_row'),
-    intro: 'This step focuses on an element',
+    title: 'Select a County 👆',
+    intro: 'You can select the county that you\'re curious about by selecting it from the dropdown. You can type the name of the county or scroll to find the county that you are interested in. Your selected county will be outlined in red on the map.',
     position: 'bottom'
   },
   {
     element: document.querySelector('#county_map_box'),
-    intro: 'This is the county map',
+    title: 'Similarity Heatmap 🗺',
+    intro: 'Once you select a county, the map will be updated automatically. Co<br><br>nties that have had the most similar COVID-19 trends to yours will be more yellow. Cou<br><br>nties that are least similar will be more purple. You can pan and zoom as you need.',
     position: 'top'
   },
   {
     element: document.querySelector('#table_similarity_box'),
-    intro: 'This is the table of similarity',
+    title: 'Similarity Table 📶',
+    intro: 'All US counties are indexed for similarity against your selection and displayed in the table by descending similarity score. A s<br><br>milarity score of 100% means that the county has had identical spread to your selected county. We included some metadata about COVID-19 spread in each county to help give context too.',
     position: 'top'
   },
   {
     element: document.querySelector('#plotly_trend_box'),
-    intro: 'This is the trend',
+    title: 'Cases per 100,000 Trended 📉',
+    intro: 'Finally, you can see an overlay of your selected county (in red) compared to the top 10 most similar counties (in light gray) based upon COVID-19 cases per hundred thousand people. You can also see the raw count by toggling the button.',
     position: 'top'
   },
   {
-    title: 'Thanks',
-    intro: 'Good Bye! 👋'
-  }
+    title: 'Share',
+   🗣  intro: 'Interested in sharing the data for your county? Simply click the "Share" button and you\'ll be provided with a link to copy to pass along to friends and family.<br>Happy <br>exploring! If you have any feedback drop a line to hello@<a href=\'mailto:hello@ddil.ai\'>ddil.ai or on</a> Twitte<a href=\'https://twitter.com/ddil_de\' target=\'_blank\'>r at @DDIL_de!'
+  }</a>
   ]
 }).oncomplete(function() {
   localStorage.setItem('doneTour', 'yeah!');
