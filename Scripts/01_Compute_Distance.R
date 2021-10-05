8a7afd7770e71b5f5471b08c302c85ab618064ed
Mehak gupta <mehakg@udel.edu>
2021-10-04 23:05:54 -0400

    test13

--- Scripts/01_Compute_Distance.R
+++ Scripts/01_Compute_Distance.R
@@ -1,7 +1,7 @@
 ## Script to compute distance
 
 library(lsa)
-
+library(dplyr)
 # Normalize 
 
 scaled_data <- 
@@ -43,9 +43,9 @@ dist_df <-
 # 
 # total_dist_cosine <- as.data.frame(total_dist_cosine)
 # 
-dist_cosine_df <-
-total_dist_cosine %>%
-  rownames_to_column("fips")
+# dist_cosine_df <-
+# total_dist_cosine %>%
+#   rownames_to_column("fips")
 
 
 ncc_ditto <-
@@ -54,20 +54,21 @@ ncc_ditto <-
   gather(comp, euc, -fips)
 
 
-ncc_ditto_cosine <-
-  dist_cosine_df %>%
-  filter(fips == 06043) %>%
-  gather(comp, cosine, -fips)
+# ncc_ditto_cosine <-
+  # dist_cosine_df %>%
+  # filter(fips == 06043) %>%
+  # gather(comp, cosine, -fips)
 
 
-cosine_euc_comp <-
-  ncc_ditto %>%
-  left_join(ncc_ditto_cosine, by = c("fips", "comp")) %>%
-  mutate(cosine = 1-cosine) %>%
-  arrange(cosine)
+# cosine_euc_comp <-
+  # ncc_ditto %>%
+  # left_join(ncc_ditto_cosine, by = c("fips", "comp")) %>%
+  # mutate(cosine = 1-cosine) %>%
+  # arrange(cosine)
 
-cosine_euc_comp %>%
-  ggplot(aes(x = euc, y = cosine)) +
-  geom_point()
+# cosine_euc_comp %>%
+  # ggplot(aes(x = euc, y = cosine)) +
+  # geom_point()
+print("end")
 
 
