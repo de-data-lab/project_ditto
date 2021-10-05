e31a48840fd0a6d2ad4a4a494c28c733c61355d4
Mehak gupta <mehakg@udel.edu>
2021-10-04 22:47:28 -0400

    test13

--- update_death_azure.R
+++ update_death_azure.R
@@ -4,8 +4,8 @@ print("Running Script to update files")
 
 library(AzureStor)
 
-#endpoint <- storage_endpoint(Sys.getenv("storage_container_url"), key = Sys.getenv("storage_container_key"))
-#container <- storage_container(endpoint, Sys.getenv("storage_container_name"))
+endpoint <- storage_endpoint('https://ddilstorage.blob.core.windows.net', key = 'DV6J/0cgBSX6k2QjjEGyZ6HP7U1hK7j6uW6BXELZPbA9/o6d4nPKlRajBdqoQW8UAc/DoRH+k4ETxyveQGZIbw==')
+container <- storage_container(endpoint, 'ddil-stg')
 
 
 #Update CSVs in Azure
